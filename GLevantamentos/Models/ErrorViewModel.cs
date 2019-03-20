16d15f3d52b24874cdf74917b175793fc92d720b
Diogo M de Oliveira <dmarostega@gmail.com>
2019-03-20 02:32:11 -0300

    Adicionar arquivos de projeto.

---
+++ GLevantamentos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GLevantamentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
